e7820686d3efca880b35bd7bd325430111cf477d
lelepods <leighalbo23@gmail.com>
2026-02-22 23:13:45 +0800

    INITIAL FRONTEND (landing page + separate adopt page)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './router.js';
+import '/public/css/global.css';
+import './styles/adopter-landingpage.css';
+import './styles/adopter-adopt.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
