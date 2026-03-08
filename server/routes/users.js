603d99bdebc34d90f145c8dddce38e0cd8749655
nunu-a <80235892+nunu-a@users.noreply.github.com>
2026-03-09 02:13:31 +0800

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,26 +1,16 @@
+// routes/users.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// GET all users
+// Admin / REST routes
 router.get('/', userController.getUsers);
-
-// GET single user by ID
 router.get('/:uid', userController.getUserById);
-
-// CREATE new user
-router.post('/', userController.createUser);
-
-// UPDATE user
 router.put('/:uid', userController.updateUser);
-
-// DELETE user
 router.delete('/:uid', userController.deleteUser);
 
-// SIGN UP
+// Public auth routes
 router.post('/signup', userController.createUser);
-
-// LOGIN
 router.post('/login', userController.loginUser);
 
 module.exports = router;
