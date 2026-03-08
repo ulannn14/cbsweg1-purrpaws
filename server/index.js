d9b027b1f863617a04b94fb98a40b576b6d0d308
nunu-a <80235892+nunu-a@users.noreply.github.com>
2026-03-09 01:05:34 +0800

    Add login route to the API
    Added login route to the API using authController.

--- server/index.js
+++ server/index.js
@@ -18,10 +18,14 @@ app.use(express.json());
 const catRoutes = require('./routes/cats');
 const userRoutes = require('./routes/users');
 const orgRoutes = require('./routes/organizations');
+const authController = require('../controllers/authController');
 
 app.use('/api/cats', catRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/organizations', orgRoutes);
+
+// Login route
+router.post('/login', authController.loginUser);
 
 // Health check endpoint
 app.get('/', (req, res) => {
