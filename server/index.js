984db4d116b078965be40143caf91079f64163bc
nunu-a <80235892+nunu-a@users.noreply.github.com>
2026-03-09 00:33:59 +0800

    Refactor index.js with comments and new routes

--- server/index.js
+++ server/index.js
@@ -7,18 +7,29 @@ const { connectDB } = require('./config/db');
 
 const app = express();
 
-// connect to PostgreSQL
+// Connect to PostgreSQL
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
+// Routes
 const catRoutes = require('./routes/cats');
+const userRoutes = require('./routes/users');
+const orgRoutes = require('./routes/organizations');
+
 app.use('/api/cats', catRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/organizations', orgRoutes);
 
+// Health check endpoint
+app.get('/', (req, res) => {
+  res.send('PurrPaws API is running');
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
