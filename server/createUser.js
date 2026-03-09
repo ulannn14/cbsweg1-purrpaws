ab502f891032603a2853b4b5a267dc7c41160cc0
ulannn14 <rlsrainmackenlhy142005@gmail.com>
2026-03-10 02:22:18 +0800

    working in local

---
+++ server/createUser.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // create province first
+  const province = await prisma.province.create({
+    data: {
+      name: "Laguna"
+    }
+  });
+
+  const hashedPassword = await bcrypt.hash("test123", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email: "test@email.com",
+      firstName: "Test",
+      lastName: "User",
+      userName: "testuser",
+      password: hashedPassword,
+      birthdate: new Date("2000-01-01"),
+      city: "Laguna",
+      provinceId: province.id,
+      address: "Sample Address"
+    }
+  });
+
+  console.log("User created:", user);
+}
+
+main().finally(() => prisma.$disconnect());
--- server/index.js
+++ server/index.js
@@ -3,12 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const { connectDB } = require('./config/db');
-
 const app = express();
-
-// Connect to PostgreSQL
-connectDB();
 
 // Middleware
 app.use(cors());
@@ -17,15 +12,9 @@ app.use(express.json());
 // Routes
 const catRoutes = require('./routes/cats');
 const userRoutes = require('./routes/users');
-//const orgRoutes = require('./routes/organizations');
-//const authController = require('../controllers/authController');
 
 app.use('/api/cats', catRoutes);
 app.use('/api/users', userRoutes);
-//app.use('/api/organizations', orgRoutes);
-
-// Login route
-//router.post('/login', authController.loginUser);
 
 // Health check endpoint
 app.get('/', (req, res) => {
@@ -34,6 +23,7 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
