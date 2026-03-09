7d8204501f638c8b356e863f748eae897c6e3181
ulannn14 <rlsrainmackenlhy142005@gmail.com>
2026-03-09 21:33:55 +0800

    changed from prisma 7 to prisma 5

--- server/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, env } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DIRECT_URL"), // CLI uses direct connection
-  },
-});
