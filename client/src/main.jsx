fca9235f8a58dafa68de29ada5c9d98ad4a6cd11
lncrlsbrt <liancarlosmbarte@gmail.com>
2026-03-13 00:22:58 +0800

    adopter apply page, !! fetching problem !! !! remove commented out sections !!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import App from './App.jsx';
 import '/public/css/global.css';
 import './styles/adopter-landingpage.css';
 import './styles/adopter-adopt.css';
+import './styles/adopter-apply.css';
 import './styles/adopter-asean.css';
 import './styles/adopter-application.css';
 import "./styles/adopter-profile.css";
