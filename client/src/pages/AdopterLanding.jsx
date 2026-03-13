41a6b21402b6aad0125ca3a20f7e52e59550a0f7
lncrlsbrt <liancarlosmbarte@gmail.com>
2026-03-13 14:55:04 +0800

    merge

--- client/src/pages/AdopterLanding.jsx
+++ client/src/pages/AdopterLanding.jsx
@@ -30,7 +30,7 @@ function AdopterLanding() {
 
     useEffect(() => {
 
-        fetch('${API}/api/pets?status=AVAILABLE&limit=4')
+        fetch(`${API}/api/pets?status=AVAILABLE&limit=4`)
         .then(res => res.json())
         .then(data => {
             console.debug("Fetched pets:", data);
